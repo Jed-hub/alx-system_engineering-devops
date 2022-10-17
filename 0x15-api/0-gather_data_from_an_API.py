76237d86692a4fe65add0202276156e5225398fa
root <jedidiabahena@gmail.com>
2022-10-17 13:57:54 -0700

    fixed

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 """script for parsing web data from an api
 """
-
-
 if __name__ == "__main__":
     import json
     import requests
     import sys
-
     base_url = 'https://jsonplaceholder.typicode.com/'
     try:
         employee_id = sys.argv[1]
@@ -19,6 +16,7 @@ if __name__ == "__main__":
     response = requests.get(url)
     user = json.loads(response.text)
     name = user[0].get('name')
+
     url = base_url + 'todos?userId={}'.format(employee_id)
     response = requests.get(url)
     objs = json.loads(response.text)
@@ -28,7 +26,9 @@ if __name__ == "__main__":
         if obj.get('completed'):
             completed_tasks.append(obj)
             completed += 1
-    print("Employee {} is done with tasks({}/{}):"
-            .format(name, completed, len(objs)))
+
+    print("Employee {} is done with tasks({}/{}):".
+        format(name, completed, len(objs)))
+
     for task in completed_tasks:
         print("\t {}".format(task.get('title')))
